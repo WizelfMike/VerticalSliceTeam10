8a164d61f16863b609e8b4404ac576b56a8651f6
WizelfMike <70898423+WizelfMike@users.noreply.github.com>
2021-12-16 10:31:26 +0100

    Succesfully imported combat and the new input system Hooray!

--- Castle Crashers/Assets/Scripts/OnlyMoving.cs
+++ Castle Crashers/Assets/Scripts/OnlyMoving.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class OnlyMoving : MonoBehaviour
 {
@@ -8,11 +9,32 @@ public class OnlyMoving : MonoBehaviour
     public float horispeed;
     public float verspeed;
 
+    //private float _moveSpeed = 10f;
+    //private Vector3 _moveVec = Vector3.zero;
+
     void Update()
     {
         float horizontal = Input.GetAxis("Horizontal") * Time.deltaTime * horispeed;
         float vertical = Input.GetAxis("Vertical") * Time.deltaTime * verspeed;
 
         transform.Translate(horizontal, 0, vertical);
+
+        //transform.Translate(_moveVec * _moveSpeed * Time.deltaTime);
+    }
+
+    void MoveUp()
+    {
+        float vertical = Input.GetAxis("Vertical") * Time.deltaTime * verspeed;
+
+        transform.Translate(0, 0, vertical);
+    }
+
+    public void OnMovement(InputValue input)
+    {
+        
+
+        //Vector2 inputVec = input.Get<Vector2>();
+
+        //_moveVec = new Vector3(inputVec.x, inputVec.y, 0);
     }
 }
